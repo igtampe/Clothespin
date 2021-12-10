f894198390e41424bb994f14a0370f10b7cc8f92
Ignacio Tampe <Igtampe@gmail.com>
2021-12-10 12:38:13 -0400

    Add A person with their log of Outfits

---
+++ Common/Tracking/LogItem.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Igtampe.Clothespin.Common.Clothes;
+
+namespace Igtampe.Clothespin.Common.Tracking {
+
+    /// <summary>A log item of an outfit worn at a specific time</summary>
+    public class LogItem:Identifiable {
+
+        /// <summary>Outfit worn on this log item</summary>
+        public Outfit? Outfit { get; set; }
+
+        /// <summary>Day this outfit was worn</summary>
+        public DateOnly? Date { get; set; }
+
+        /// <summary>Owner of this item</summary>
+        public Person? Owner { get; set; }
+
+        /// <summary>Note for this log item</summary>
+        public string Note { get; set; } = "";
+
+    }
+}
---
+++ Common/Tracking/Person.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Igtampe.Clothespin.Common.Clothes.Items;
+using Igtampe.Clothespin.Common.Clothes;
+
+namespace Igtampe.Clothespin.Common.Tracking {
+
+    /// <summary>Represents a person who owns multiple clothing items, and outfits</summary>
+    public class Person : Identifiable, Nameable {
+
+        /// <summary>Name of the person</summary>
+        public string Name { get; set; } = "";
+
+        /// <summary>A person's Tied User account</summary>
+        public User? TiedUser { get; set; }
+
+        /// <summary>Outfits this person has</summary>
+        public List<Outfit> Outfits { get; set; } = new();
+
+        /// <summary>List of log items of Outfits worn</summary>
+        public List<LogItem> Log { get; set; } = new();
+
+        /// <summary>Accessories this person has</summary>
+        public List<Accessory> Accessories { get; set; } = new();
+
+        /// <summary>Belts this person has</summary>
+        public List<Belt> Belts { get; set; } = new();
+
+        /// <summary>Overshirts this person has</summary>
+        public List<Overshirt> Overshirts { get; set; } = new();
+
+        /// <summary>Pants this person has</summary>
+        public List<Pants> Pants { get; set; } = new();
+
+        /// <summary>Shirts this person has</summary>
+        public List<Shirt> Shirts { get; set; } = new();
+
+        /// <summary>Shoes this person has</summary>
+        public List<Shoes> Shoes { get; set; } = new();
+
+        /// <summary>Socks this person has</summary>
+        public List<Socks> Socks { get; set; } = new();
+    }
+}
