82aa5b9b40e48d3df53884afc2ac5ef24692e4ea
Ignacio Tampe <Igtampe@gmail.com>
2021-12-10 21:33:06 -0400

    Add API Project (Standalone beta)

---
+++ API/Program.cs
@@ -0,0 +1,45 @@
+using Microsoft.OpenApi.Models;
+using Igtampe.Clothespin.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options => {
+    options.SwaggerDoc("BETA_STANDALONE", new OpenApiInfo {
+        Version = "BETA_STANDALONE", Title = "Clothespin API",
+        Description = "An API For Clothespin",
+        //TermsOfService = new Uri("https://example.com/terms"),
+        Contact = new OpenApiContact {
+            Name = "Chopo",
+            Url = new Uri("https://twitter.com/igtampe"),
+            Email = "igtampe@gmail.com",
+        },
+        License = new OpenApiLicense {
+            Name = "CC0",
+            //Url = new Uri("https://example.com/license") //TODO: Actually specify the license once this is done
+        }
+    });
+});
+
+builder.Services.AddDbContext<ClothespinContext>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment()) {
+    app.UseSwagger();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/BETA_STANDALONE/swagger.json", "Clothespin API"));
+    app.UseDeveloperExceptionPage();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
