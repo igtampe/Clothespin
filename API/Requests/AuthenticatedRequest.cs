db72d91e2640b7585f97ccde0b621db7f64120e8
Ignacio Tampe <Igtampe@gmail.com>
2021-12-10 21:33:59 -0400

    Add some requests for the backend

---
+++ API/Requests/AuthenticatedRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace Igtampe.Clothespin.API.Requests {
+
+    /// <summary>A request that is Authenticated</summary>
+    public abstract class AuthenticatedRequest {
+
+        /// <summary>GUID of the Session that sent this request</summary>
+        public Guid SessionID { get; set; }
+    }
+}
---
+++ API/Requests/ChangePasswordRequest.cs
@@ -0,0 +1,12 @@
+﻿namespace Igtampe.Clothespin.API.Requests {
+
+    /// <summary>Authenticated request to change a password</summary>
+    public class ChangePasswordRequest : AuthenticatedRequest {
+
+        /// <summary>Current password</summary>
+        public string? Current { get; set; }
+
+        /// <summary>New Password</summary>
+        public string? New { get; set; }
+    }
+}
