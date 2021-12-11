24a9460658028e037d779fe2414291348f9fb9e5
Ignacio Tampe <Igtampe@gmail.com>
2021-12-10 21:32:26 -0400

    Import Cleanup, switch back to DbSet non-nullable

--- Data/ClothespinContext.cs
+++ Data/ClothespinContext.cs
@@ -1,14 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Igtampe.Clothespin.Common;
+﻿using Igtampe.Clothespin.Common;
 using Igtampe.Clothespin.Common.Tracking;
 using Igtampe.Clothespin.Common.Clothes;
 using Igtampe.Clothespin.Common.Clothes.Items;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Metadata;
 
 namespace Igtampe.Clothespin.Data {
     /// <summary>Holds the general purpose Clothespin context</summary>
@@ -21,30 +15,32 @@ namespace Igtampe.Clothespin.Data {
         /// <returns></returns>
         private static string DefaultURL() => File.Exists("csdb.txt") ? File.ReadAllText("csdb.txt") : DEFAULT_URL;
 
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
         public ClothespinContext() : base(DefaultURL()) { }
 
         public ClothespinContext(ContextMode Mode, string? URL) : base(Mode, URL ?? DefaultURL()) { }
+#pragma warning restore CS8618 // DbSets are marked as potentially null but it doesn't matter here because it will need to be configured later!!!
 
-        public DbSet<User>? User { get; set; }
+        public DbSet<User> User { get; set; }
 
-        public DbSet<Person>? Person { get; set; }
+        public DbSet<Person> Person { get; set; }
 
-        public DbSet<LogItem>? LogItem { get; set; }
+        public DbSet<LogItem> LogItem { get; set; }
 
-        public DbSet<Outfit>? Outfit { get; set; }
+        public DbSet<Outfit> Outfit { get; set; }
         
-        public DbSet<Accessory>? Accessory { get; set; }
+        public DbSet<Accessory> Accessory { get; set; }
 
-        public DbSet<Belt>? Belt { get; set; }
+        public DbSet<Belt> Belt { get; set; }
 
-        public DbSet<Overshirt>? Overshirt { get; set; }
+        public DbSet<Overshirt> Overshirt { get; set; }
 
-        public DbSet<Pants>? Pants { get; set; }
+        public DbSet<Pants> Pants { get; set; }
 
-        public DbSet<Shirt>? Shirt { get; set; }
+        public DbSet<Shirt> Shirt { get; set; }
 
-        public DbSet<Shoes>? Shoes { get; set; }
+        public DbSet<Shoes> Shoes { get; set; }
 
-        public DbSet<Socks>? Socks { get; set; }
+        public DbSet<Socks> Socks { get; set; }
     }
 }
