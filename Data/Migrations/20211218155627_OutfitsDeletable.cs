06c6c75f59a878c4bb1e0a143dc27e343e7eb91f
Ignacio Tampe <Igtampe@gmail.com>
2021-12-18 11:56:58 -0400

    Data migration ot make Outfits deltable

---
+++ Data/Migrations/20211218155627_OutfitsDeletable.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Data.Migrations
+{
+    public partial class OutfitsDeletable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Deleted",
+                table: "Outfit",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Deleted",
+                table: "Outfit");
+        }
+    }
+}
