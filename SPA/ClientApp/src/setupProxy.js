1f37b383c629ab259d492ecc9a6a0c83224851ae
Ignacio Tampe <Igtampe@gmail.com>
2021-12-31 12:43:52 -0400

    Proxy configured
    We should be good to go now. OK que lindo.

--- SPA/ClientApp/src/setupProxy.js
+++ SPA/ClientApp/src/setupProxy.js
@@ -5,7 +5,9 @@ const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_H
   env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'http://localhost:34989';
 
 const context =  [
-  "/weatherforecast",
+    "/weatherforecast",
+    "/API",
+    "/Swagger"
 ];
 
 module.exports = function(app) {
