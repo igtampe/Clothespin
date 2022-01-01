70c66f5d02565e5bc63dd560740ec82c6e134bda
Ignacio Tampe <Igtampe@gmail.com>
2022-01-01 00:29:52 -0400

    Add themes and some routes

--- SPA/ClientApp/src/App.js
+++ SPA/ClientApp/src/App.js
@@ -6,24 +6,39 @@ import Login from './components/Login'
 import { FetchData } from './components/FetchData';
 import { Counter } from './components/Counter';
 import Cookies from 'universal-cookie';
+import { MuiThemeProvider, createTheme } from '@material-ui/core/styles';
 
 import './custom.css'
 
 const cookies = new Cookies();
+
+const theme = createTheme({
+  palette: {
+    primary : {
+      main: '#E1DBB9'
+    },
+    secondary: {
+      main: '#5E5B4E'
+    }
+  }
+});
 
 export default class App extends Component {
   static displayName = App.name;
 
   render () {
     return (
+      <MuiThemeProvider theme={theme}>
       <Layout>
-        <Route exact path='/'>
-          cookies.get("SessionID")===undefined ? <Redirect to='/Login'/> : Home
+        <Route path='/'>
+          {cookies.get("SessionID")===undefined ? <Redirect to='/Login'/> : <Redirect to='/Home'/>}
         </Route>
-        <Route exact path='/Login' component={Login} />
+        <Route path='/Home' component={Home}/>
+        <Route path='/Login' component={Login} />
         <Route path='/counter' component={Counter} />
         <Route path='/fetch-data' component={FetchData} />
       </Layout>
+      </MuiThemeProvider>
     );
   }
 }
