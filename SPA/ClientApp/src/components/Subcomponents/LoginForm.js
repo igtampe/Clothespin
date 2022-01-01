f5a9a0ae32d0921d611f104124df4473979feaec
Ignacio Tampe <Igtampe@gmail.com>
2022-01-01 00:30:16 -0400

    Modify login form for Clothespin

--- SPA/ClientApp/src/components/Subcomponents/LoginForm.js
+++ SPA/ClientApp/src/components/Subcomponents/LoginForm.js
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import TextField from '@material-ui/core/TextField';
-import Grid from "@material-ui/core/Grid"
 import Button from "@material-ui/core/Button"
 import Typography from "@material-ui/core/Typography";
 import { CircularProgress, Container } from "@material-ui/core";
@@ -11,6 +10,7 @@ import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import Cookies from 'universal-cookie';
+import { useHistory } from "react-router-dom";
 
 // react.school/material-ui
 
@@ -31,50 +31,99 @@ const cookies = new Cookies();
 
 export default function LoginForm() {
     const classes = useStyles();
+    const history = useHistory();
 
     const [Pin, SetPin] = useState("");
     const [ID, SetID] = useState("");
     const [LoginInProgress, SetLoginInProgress] = useState(false);
     const [ResultOpen, setResultOpen] = useState(false);
-    const [ResultText, setResultText] = useState("IDK what happened I wasn't set");
+    const [Result, setResult] = useState({
+        Action: "do something",
+        Text: "Desconozco"
+    });
 
     const handleIDChange = (event) => { SetID(event.target.value); };
     const handlePinChange = (event) => { SetPin(event.target.value); };
     const handleCloseResultDialog = () => { setResultOpen(false); }
 
-    const OnLoginButtonClick = (event) => { 
-        SetLoginInProgress(true); 
+    const OnLoginButtonClick = (event) => {
+        SetLoginInProgress(true);
 
         //Grab the ID and pin and create a tiny itty bitty object
         const requestOptions = {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ "id":ID, "pin":Pin })
+            body: JSON.stringify({ "username": ID, "password": Pin })
         };
 
         console.log(requestOptions.body);
 
-        fetch("Auth/", requestOptions)
+        fetch("API/Users", requestOptions)
             .then(response => {
                 SetLoginInProgress(false);
-                if(!response.ok){
-                    setResultText(response.statusText);
+                if (!response.ok) {
+                    setResult({ Action: "Login", Text: response.statusText });
                     setResultOpen(true);
                 }
                 return response.json()
             }).then(data => {
                 console.log(data)
-                if(data===undefined){
-                } else if(ResultOpen){
-                    setResultText(data);
-                } else if(data==="00000000-0000-0000-0000-000000000000"){
-                    setResultText("Username or password is incorrect");
+                if (data === undefined) {
+                } else if (ResultOpen) {
+                    setResult({ Action: "Login", Text: data });
+                } else if (data === "00000000-0000-0000-0000-000000000000") {
+                    setResult({ Action: "Login", Text: "Username or password is incorrect" });
                     setResultOpen(true);
                 } else {
                     //We logged in, save a cookie, then let's get the heck out of here
-                    cookies.set('SessionID', data, { path: '/' })
-                    
+                    cookies.set('SessionID', data, { path: '/', maxAge: 60 * 60 * 24 }) //The cookie will expire in a day
+                    history.go("/Home")
                 }
+            })
+    }
+
+    const OnRegisterButtonClick = (event) => {
+
+        //Check that the ID and Pin are *not* empty
+        if (ID === undefined || ID === "") {
+            setResult({ Action: "Register", Text: "Username cannot be empty!" });
+            setResultOpen(true);
+            return;
+        }
+
+        if (Pin === undefined || Pin === "") {
+            setResult({ Action: "Register", Text: "Pin cannot be empty!" });
+            setResultOpen(true);
+            return;
+        }
+
+        SetLoginInProgress(true);
+
+        //Now actually try this
+        const requestOptions = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ "username": ID, "password": Pin })
+        };
+
+        console.log(requestOptions.body);
+
+        fetch("API/Users/register", requestOptions)
+            .then(response => {
+                SetLoginInProgress(false);
+                if (!response.ok) {
+                    setResult({ Action: "Register", Text: response.statusText });
+                    setResultOpen(true);
+                }
+                return response.text();
+            }).then(data => {
+                console.log(data)
+                if (data === undefined) {
+                } else if (data === "") {
+                    //We have successfully registered. Now try to login
+                    OnLoginButtonClick();
+                    return;
+                } else { setResult({ Action: "Register", Text: data }); }
             })
     }
 
@@ -82,28 +131,35 @@ export default function LoginForm() {
         <React.Fragment>
             <Container style={{ backgroundColor: '#ebebeb', padding: '50px' }}>
                 <Typography>
-                    <Typography variant="h6" className={classes.title}> Log In to Calico</Typography>
-                    <Grid container spacing={3} style={{ paddingTop: "20px", paddingBottom: "20px" }}>
-                        <Grid item xs={4}>
-                            <TextField label="Neco ID" value={ID} onChange={handleIDChange} />
-                        </Grid>
-                        <Grid item xs={4}>
-                            <TextField label="Pin" value={Pin} type="password" onChange={handlePinChange} />
-                        </Grid>
-                    </Grid>
-                    <div style={{ textAlign: 'right' }}>
-                        <Button variant='contained' color='primary' disabled={LoginInProgress} onClick={OnLoginButtonClick}>
-                            {LoginInProgress ? <CircularProgress size={20} /> : "Log In"
-                            }
+
+                    <Typography variant="h6" className={classes.title}
+                        style={{ fontFamily: "DM Serif Display", textAlign: "center" }}>
+                        <img src="icon.png" alt="Logo" width="200" /><br />Welcome to Clothespin
+                    </Typography>
+                    <TextField label="Username" value={ID} onChange={handleIDChange} fullWidth
+                        style={{ marginTop: "5px", marginBottom: "5px" }} /><br />
+                    <TextField label="Password" value={Pin} type="password" onChange={handlePinChange} fullWidth
+                        style={{ marginTop: "5px", marginBottom: "5px" }} /><br />
+
+                    <br />
+
+                    <div style={{ textAlign: 'center' }}>
+                        <Button variant='contained' color='primary' disabled={LoginInProgress} onClick={OnLoginButtonClick}
+                            style={{ margin: "10px" }}>
+                            {LoginInProgress ? <CircularProgress size={20} /> : "Log In"}
+                        </Button> or
+                        <Button variant='contained' color='secondary' disabled={LoginInProgress} onClick={OnRegisterButtonClick}
+                            style={{ margin: "10px" }}>
+                            {LoginInProgress ? <CircularProgress size={20} /> : "Register"}
                         </Button>
                     </div>
                 </Typography>
             </Container>
 
             <Dialog open={ResultOpen} onClose={handleCloseResultDialog} >
-                <DialogTitle> Could not log in </DialogTitle>
+                <DialogTitle> Could not {Result.Action} </DialogTitle>
                 <DialogContent>
-                    <DialogContentText>{ResultText}</DialogContentText>
+                    <DialogContentText>{Result.Text}</DialogContentText>
                 </DialogContent>
                 <DialogActions>
                     <Button onClick={handleCloseResultDialog} autoFocus> OK </Button>
