02e6876f26c901e4ce06d9b2e4e1f59dd02ff7eb
Ignacio Tampe <Igtampe@gmail.com>
2022-01-03 03:04:25 -0400

    Add a footer to the pages

--- SPA/ClientApp/src/components/Layout.js
+++ SPA/ClientApp/src/components/Layout.js
@@ -1,3 +1,4 @@
+import { Divider } from '@material-ui/core';
 import React, { Component } from 'react';
 import NavMenu from './NavMenu';
 
@@ -10,6 +11,12 @@ export class Layout extends Component {
         <NavMenu />
         <div style={{marginLeft:"30px", marginRight:"30px"}}>
           {this.props.children}
+          <Divider style={{marginTop:"20px"}}/>
+          <div style={{color:"#B0B0B0", fontSize:".7em", marginTop:"20px", marginLeft:"25px", marginRight:"25px", textAlign:'center'}}>
+            ©2022 <a href='https://sites.google.com/view/igtampe' style={{color:"#B0B0B0"}}>Igtampe</a>, no rights reserved<br/>
+            Built using ReactJS and ASP.NET<br/>
+            See the <a href='https://www.github.com/igtampe/clothespin' style={{color:"#B0B0B0"}}>Github</a>
+            </div>
         </div>
       </div>
     );
