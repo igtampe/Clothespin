426fc7c55ff2321764a2ccd2fc716c30e9f75a44
Ignacio Tampe <Igtampe@gmail.com>
2022-01-03 03:02:52 -0400

    Do the home

--- SPA/ClientApp/src/components/Home.js
+++ SPA/ClientApp/src/components/Home.js
@@ -1,14 +1,28 @@
-import React, { Component } from 'react';
+import { Button, Container, Grid } from "@material-ui/core";
+import React from "react";
+import useWindowDimensions from "./Hooks/useWindowDimensions";
+import { useHistory } from "react-router-dom";
 
-export class Home extends Component {
-  static displayName = Home.name;
+export default function HomeComponent() {
 
-  render () {
-    return (
-      <div>
-        <h1>Coming Soon</h1>
-        <p>We need to work on this homepage....</p>
-      </div>
-    );
-  }
+  const { height, width } = useWindowDimensions();
+
+  const history = useHistory();
+
+  var Vertical = width < 900;
+
+  const sendToLogin = (event) => { history.push("/Login") }
+
+  return (
+    <React.Fragment>
+      <Container style={{ background: "#e0e0e0", padding: "50px", verticalAlign: "middle" }}>
+        {!Vertical ? <img src="/homeimage.png" width="100%" style={{ marginBottom: "20px" }} /> : ""}
+        <div style={{ fontFamily: 'DM Serif Display' }}><h1><b>Welcome to Clothespin</b></h1></div>
+        <p> Clothespin is a comprehensive system to help you track how much you use your clothes. Simply register your closet,
+          your most common outfits, and with a bit of daily logging and some time, gain insights into your clothing usage.</p>
+        <div style={{ textAlign: "right", marginTop: "0px" }}><Button onClick={sendToLogin} variant="contained" color="primary">Get Started</Button></div>
+      </Container>
+    </React.Fragment>
+  );
+
 }
