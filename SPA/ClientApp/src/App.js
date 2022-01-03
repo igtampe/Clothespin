113d1760c7e258067d6285e7aa7bd89e453a0370
Ignacio Tampe <Igtampe@gmail.com>
2022-01-03 03:04:49 -0400

    Fix the import to home

--- SPA/ClientApp/src/App.js
+++ SPA/ClientApp/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Route, Redirect } from 'react-router';
 import { Layout } from './components/Layout';
-import { Home } from './components/Home';
+import Home from './components/Home';
 import Login from './components/Login'
 import { FetchData } from './components/FetchData';
 import { Counter } from './components/Counter';
