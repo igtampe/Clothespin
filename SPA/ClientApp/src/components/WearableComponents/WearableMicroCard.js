218af9e2066056777800dfb9665752578796d5bf
Ignacio Tampe <Igtampe@gmail.com>
2022-01-04 21:34:26 -0400

    Add Wearable microcards and picker for outfit creation and editing

---
+++ SPA/ClientApp/src/components/WearableComponents/WearableMicroCard.js
@@ -0,0 +1,138 @@
+import React, { useState } from "react";
+import {
+    Accordion, AccordionDetails, AccordionSummary, FormControl,
+    InputLabel, Select, MenuItem, Snackbar, Button, Grid
+} from "@material-ui/core";
+import { ExpandMore } from '@material-ui/icons'
+import Cookies from 'universal-cookie';
+import { Alert } from "reactstrap";
+
+const cookies = new Cookies();
+
+export default function WearableMicroCard(props) {
+
+    const [expanded, setExpanded] = useState(false)
+    const [Wearable, setWearable] = useState(props.wearable)
+
+    const [result, setResult] = useState({ severity: "success", text: "idk" })
+    const [SnackOpen, setSnackOpen] = useState(false);
+
+    const onChangeWashState = (event) => {
+
+        var value = event.target.value;
+        if (value === undefined) {
+
+            if (event.target.innerText === "MARK DIRTY") {
+                console.log("Dirty!")
+                value = 2
+            }
+            else if (event.target.innerText === "MARK CLEAN") {
+                console.log("Clean!")
+                value = 0
+            }
+            else {
+                console.log(event);
+                setResult({
+                    severity: "danger",
+                    text: "Unknown target washstate"
+                })
+
+                setSnackOpen(true);
+                return;
+            }
+        }
+
+        //Send the request 
+        const requestOptions = {
+            method: 'PUT',
+            headers: { 'Content-Type': 'application/json', 'SessionID': cookies.get('SessionID') },
+            body: JSON.stringify({ ...Wearable, "state": value })
+        };
+
+        var url = "API/Clothes/" + props.type
+            + "?ID=" + Wearable.id
+
+        fetch(url,
+            requestOptions)
+            .then(response => {
+                if (!response.ok) {
+                    console.error(response);
+                    return undefined
+                }
+                return response.json()
+            }).then(data => {
+                if (data === undefined) {
+
+                    setResult({
+                        severity: "danger",
+                        text: "Could not update wash state for this item"
+                    })
+
+                    setSnackOpen(true);
+
+                }
+                else {
+                    setWearable(data)
+                    setResult({
+                        severity: "success",
+                        text: Wearable.name + "'s wash state has been updated"
+                    })
+                    setSnackOpen(true);
+                }
+            })
+    }
+
+    const toggleExpanded = (event) => { setExpanded(!expanded); }
+
+    return (
+        <React.Fragment>
+            <Accordion hidden={!Wearable ? false : Wearable.deleted} expanded={!props.washable ? false : expanded} 
+            onChange={!props.washable ? undefined : toggleExpanded} style={{ borderRadius: "10px", border: "2px solid #e0e0e0", maxWidth: "300px" }}>
+                <AccordionSummary expandIcon={!props.washable? undefined :<ExpandMore />}>
+                    <table>
+                        <tr>
+                            <td style={{ minWidth: "80x" }}><img style={{ margin: "5px" }} src={Wearable === undefined ? "/images/" + props.type + ".png" :
+                                Wearable.imageURL === "" || Wearable.imageURL === undefined ? "/images/" + props.type + ".png" : Wearable.imageURL
+                            } alt={props.type} width="50px" /></td>
+                            <td style={{ minWidth: "155px" }}>
+                                <div style={{ margin: "5px", textAlign: "left", lineHeight: "1.1em" }}>
+                                    <b>{Wearable === undefined ? props.type : Wearable.name}</b>
+                                </div>
+                            </td>
+                        </tr>
+                    </table>
+                </AccordionSummary>
+                <AccordionDetails>
+                    <Grid container spacing={2}>
+                    <Grid item xs={12}>
+                            {Wearable && (Wearable.state === 0 || Wearable.state === 1)
+                                ? <Button fullWidth id="DirtyBTN" onClick={onChangeWashState} variant="contained" color="secondary">Mark Dirty</Button>
+                                : <Button fullWidth id="CleanBTN" onClick={onChangeWashState} variant="contained" color="primary">Mark Clean</Button>}
+                        </Grid>
+                        <Grid item xs={12}>
+                            <FormControl fullWidth>
+                                <InputLabel fullWidth>Wash State</InputLabel>
+                                <Select fullWidth label="Label" value={Wearable === undefined ? 0 : Wearable.state}
+                                    disabled={!Wearable} onChange={onChangeWashState}>
+                                    <MenuItem value={0}>Clean</MenuItem>
+                                    <MenuItem value={1}>Semi-Clean</MenuItem>
+                                    <MenuItem value={2}>Dirty</MenuItem>
+                                    <MenuItem value={3}>Washing</MenuItem>
+                                    {/**<MenuItem value={4}Drying</MenuItem>**/}
+                                    <MenuItem value={5}>Washed</MenuItem>
+                                </Select></FormControl>
+                        </Grid>
+                    </Grid>
+                </AccordionDetails>
+            </Accordion>
+
+            <Snackbar open={SnackOpen} autoHideDuration={6000} onClose={() => setSnackOpen(false)}>
+                <Alert onClose={() => setSnackOpen(false)} color={result.severity} sx={{ width: '100%' }}>
+                    {result.text}
+                </Alert>
+            </Snackbar>
+
+        </React.Fragment>
+    );
+
+}
---
+++ SPA/ClientApp/src/components/WearableComponents/WearablePicker.js
@@ -0,0 +1,227 @@
+import { Button, CircularProgress, Grid, IconButton, TextField } from "@material-ui/core";
+import React, { useState } from "react";
+import Cookies from 'universal-cookie';
+import { SortSelect, SubSelect, WashSelect } from "./WearableDisplay";
+import SearchIcon from "@material-ui/icons/Search"
+
+const cookies = new Cookies();
+
+export default function WearablePicker(props) {
+
+    const [query, setQuery] = useState("");
+    const [sortOrder, setSortOrder] = useState(0);
+    const [subType, setSubType] = useState(-1);
+    const [washState, setWashState] = useState(-1); 
+    const [subTypes, setSubTypes] = useState(undefined);
+
+    const [displayedItems, setDisplayedItems] = useState(0);
+    const [noMas, setNoMas] = useState(false)
+    const [gettingMas, setGettingMas] = useState(false)
+
+    const [subLoading, setSubLoading] = useState(false);
+    const [colLoading, setColLoading] = useState(false)
+
+    const [collection, setCollection] = useState(undefined)
+
+    if (subTypes === undefined && !subLoading) {
+
+        setSubLoading(true)
+
+        //Get the shirt subtypes
+        fetch("/API/Clothes/Types/" + props.types)
+            .then((response) => response.json())
+            .then((data) => {
+                setSubTypes(data)
+                setSubLoading(false);
+            })
+    }
+
+    if (collection === undefined && setCollection !== undefined && !colLoading) {
+
+        //Time to get this collection
+        setColLoading(true)
+
+        ///API/Persons/Clothes/Shirts?PersonID=38a015f4-388e-4866-030c-08d9c23a261d&Sort=0&Query=a&Type=0&State=0
+        const requestOptions = {
+            method: 'GET',
+            headers: { 'Content-Type': 'application/json', 'SessionID': cookies.get('SessionID') },
+        };
+
+        console.log(requestOptions.headers);
+        var url = "API/Persons/Clothes/" + props.types
+            + "?PersonID=" + cookies.get("PersonID")
+            + "&Sort=" + sortOrder
+            + "&Query=" + query
+            + (subType === -1 ? "" : "&Type=" + subType)
+            + (washState === -1 ? "" : "&State=" + washState)
+
+        console.log(url)
+
+        fetch(url,
+            requestOptions)
+            .then(response => {
+                if (!response.ok) {
+                    console.error(response);
+                    return undefined
+                }
+                return response.json()
+            }).then(data => {
+                console.log(data)
+                if (data === undefined) { } else {
+                    setCollection(data)
+                    setDisplayedItems(20);
+                    setNoMas(data.length !== 20)
+                }
+                setColLoading(false)
+            })
+    }
+
+    const getMas = (event) => {
+        setGettingMas(true)
+
+        const requestOptions = {
+            method: 'GET',
+            headers: { 'Content-Type': 'application/json', 'SessionID': cookies.get('SessionID') },
+        };
+
+        console.log(requestOptions.headers);
+        var url = "API/Persons/Clothes/" + props.types
+            + "?PersonID=" + cookies.get("PersonID")
+            + "&Sort=" + sortOrder
+            + (query !== undefined && query !== "" ? "&Query=" + query : "")
+            + (subType === -1 ? "" : "&Type=" + subType)
+            + (washState === -1 ? "" : "&State=" + washState)
+            + "&Skip=" + displayedItems
+
+        console.log(url)
+
+        fetch(url,
+            requestOptions)
+            .then(response => {
+                setGettingMas(false)
+                if (!response.ok) {
+                    console.error(response);
+                    return undefined
+                }
+                return response.json()
+            }).then(data => {
+                console.log(data)
+                if (data === undefined) { } else {
+                    setCollection(collection.concat(data))
+                    setDisplayedItems(displayedItems + 20);
+                    setNoMas(data.length !== displayedItems)
+                }
+            })
+    }
+
+    const handleSelect = (wearable, wasSelected) => (event) => {
+
+        console.log("selected " + wearable.name)
+        console.log("WasSelected? " + wasSelected)
+
+        if(!props.isCollection){
+            if(wasSelected) {props.setSelected(undefined)} 
+            else {props.setSelected(wearable)}
+            
+            if(props.advanceAccordion && !wasSelected){props.advanceAccordion()}
+        } else {
+
+            var selected = [...props.selected]
+
+            if(wasSelected) {selected.splice(selected.indexOf(wearable.id),1)} 
+            else { selected.push(wearable.id)}
+            console.log(selected)
+            props.setSelected(selected) //Refresh
+        }
+        
+    }
+
+    return (
+        <React.Fragment>
+            <Grid container spacing={2}>
+                <Grid item xs={11}>
+
+                    <TextField label="Search" value={query} onChange={(event) => setQuery(event.target.value)} fullWidth
+                        style={{ marginTop: "5px", marginBottom: "5px" }} />
+
+                </Grid>
+                <Grid item xs={1}>
+                    <IconButton onClick={() => setCollection(undefined)}><SearchIcon /></IconButton>
+                </Grid>
+                {
+                    props.vertical
+                        ? <>
+                            <Grid item xs={12}>
+                                <SortSelect sortOrder={sortOrder} setSortOrder={setSortOrder} setCollection={setCollection} />
+                                {subTypes === undefined ? "" : <SubSelect subType={subType} setSubType={setSubType} subTypes={subTypes} setCollection={setCollection} />}
+                                {props.washable ? <WashSelect washState={washState} setWashState={setWashState} setCollection={setCollection} /> : ""}
+                            </Grid>
+                        </>
+                        : <>
+                            <Grid item xs={4}>
+                                <SortSelect sortOrder={sortOrder} setSortOrder={setSortOrder} setCollection={setCollection} />
+                            </Grid>
+                            <Grid item xs={4}>
+                                {subTypes === undefined ? "" : <SubSelect subType={subType} setSubType={setSubType} subTypes={subTypes} setCollection={setCollection} />}
+                            </Grid>
+                            <Grid item xs={4}>
+                                {props.washable ? <WashSelect washState={washState} setWashState={setWashState} setCollection={setCollection} /> : ""}
+                            </Grid>
+
+                        </>
+                }
+                <Grid item xs={12}>
+                    <div>
+                        {
+                            collection === undefined
+                                ? <div style={{ textAlign: "center" }}> <CircularProgress /></div>
+                                : <>{
+                                    collection.length === 0
+                                        ? <div style={{ textAlign: "center" }}> No wearables of this type </div>
+                                        : <>
+                                            <br />
+                                            <Grid container spacing={2}>
+                                                {collection.map(Wearable => {
+                                                    
+                                                    var isSelected = false
+                                                    if(props.selected !== undefined){
+                                                        if(!props.isCollection){ isSelected=(props.selected.id===Wearable.id) }
+                                                        else { isSelected = props.selected.includes(Wearable.id)}
+                                                    }
+
+                                                    return(<>
+                                                    <Grid item xs>
+                                                    <Button style={isSelected ? {backgroundColor:'#e0ffc7'} : {}} 
+                                                    variant={ isSelected ? 'contained' : 'outlined'} onClick={handleSelect(Wearable,isSelected)}>
+                                                        <table style={{minHeight:"100px", minWidth:"250px", maxWidth:"250px"}}>
+                                                            <tr>
+                                                                <td style={{ minWidth: "80x", maxWidth:"80px" }}>
+                                                                    <img style={{ margin: "5px" }} src={Wearable === undefined ? "/images/" + props.type + ".png" :
+                                                                    Wearable.imageURL === "" || Wearable.imageURL === undefined ? "/images/" + props.type + ".png" : Wearable.imageURL } 
+                                                                    alt={props.type} width="75px" />
+                                                                </td>
+                                                                <td style={{ minWidth: "170x", maxWidth:"170px" }}>
+                                                                    <div style={{ margin: "5px", textAlign: "left", lineHeight: "1.1em" }}>
+                                                                        <b>{Wearable === undefined ? props.type : Wearable.name}</b><br />
+                                                                        <i>{Wearable === undefined || subTypes === undefined ? "Unknown" : subTypes[Wearable.type].replace("_", " ")}</i>
+                                                                    </div>
+                                                                </td>
+                                                            </tr>
+                                                        </table>
+                                                    </Button>
+                                                    </Grid>
+                                                </>)})}
+                                            </Grid>
+                                            <div style={{ textAlign: "center", marginTop: "10px" }}>
+                                                <Button hidden={noMas} disabled={gettingMas} color="primary" onClick={getMas}>{gettingMas ? <CircularProgress /> : "Load more"}</Button>
+                                            </div>
+                                        </>
+                                }</>
+                        }
+                    </div>
+                </Grid>
+            </Grid>
+        </React.Fragment>
+    );
+
+}
