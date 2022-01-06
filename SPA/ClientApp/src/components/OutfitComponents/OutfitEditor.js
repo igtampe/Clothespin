360ea37309850901428620898047fcb3cb3fe85f
Ignacio Tampe <Igtampe@gmail.com>
2022-01-05 20:19:45 -0400

    Move display of wearable microcards to the editor

--- SPA/ClientApp/src/components/OutfitComponents/OutfitEditor.js
+++ SPA/ClientApp/src/components/OutfitComponents/OutfitEditor.js
@@ -1,7 +1,8 @@
-import { Button, Dialog, DialogActions, DialogContent, DialogTitle, Snackbar, TextField } from "@material-ui/core";
+import { Button, Dialog, DialogActions, DialogContent, DialogTitle, Divider, Grid, Snackbar, TextField } from "@material-ui/core";
 import React, { useState } from "react";
 import { Alert } from "reactstrap";
 import Cookies from 'universal-cookie';
+import WearableMicroCard from "../WearableComponents/WearableMicroCard";
 
 const cookies = new Cookies();
 
@@ -14,19 +15,60 @@ export default function OutfitEditor(props) {
     const [populated, setPopulated] = useState(false);
 
     const [result, setResult] = useState({ severity: "success", text: "idk" })
-
     const [SnackOpen, setSnackOpen] = useState(false);
+
+    const setShirt = (shirt) => { props.setOutfit({ ...props.outfit, shirt: shirt }) }
+    const setPants = (pants) => { props.setOutfit({ ...props.outfit, pants: pants }) }
+    //const setBelt = (belt) => { props.setOutfit({...props.outfit, belt:belt})}
+    const setSocks = (socks) => { props.setOutfit({ ...props.outfit, socks: socks }) }
+    //const setShoes = (shoes) => { props.setOutfit({...props.outfit, shoes:shoes})}
+
+    const setOvershirt = (overshirt) => {
+
+        //find the index of the overshirt
+        var IndexOf = -1
+
+        //Make a copy since we're going to need to anyway
+        var OS = [...props.outfit.overshirts]
+
+        //Find it
+        for (let i = 0; i < OS.length; i++) {
+            if (OS[i] && OS[i].id && OS[i].id === overshirt.id) {
+                IndexOf = i; //damn a For loop on the frontend??? 
+                break;
+            }
+        }
+
+        console.log(IndexOf)
+
+        //If it was not found just return
+        if (IndexOf === -1) { return; }
+
+        //Replace the overshirt
+        OS[IndexOf] = overshirt
+
+        //and update
+        props.setOutfit({ ...props.outfit, overshirts: OS })
+
+    }
+
+    //We don't actually need to do setAccessory because there's no time that accessories would be updated
+    //along with shoes and belt
 
     if (props.outfit && !populated && props.open) {
         //Populate
         setPopulated(true);
+
         setName(props.outfit.name)
+        setImageURL(props.outfit.imageURL)
         setDescription(props.outfit.description)
     }
 
     const ClearForm = () => {
         setName("")
         setDescription("")
+        setImageURL("")
+
         setPopulated(false)
     }
 
@@ -60,7 +102,7 @@ export default function OutfitEditor(props) {
         requestOptions = {
             ...requestOptions,
             headers: { 'Content-Type': 'application/json', 'SessionID': cookies.get('SessionID') },
-            body: JSON.stringify({ id: props.outfit.id, name: name, description: description, imageURL:imageURL })
+            body: JSON.stringify({ id: props.outfit.id, name: name, description: description, imageURL: imageURL })
         }
 
         fetch(url, requestOptions)
@@ -78,7 +120,7 @@ export default function OutfitEditor(props) {
 
                 } else {
                     setResult({ severity: "success", text: name + " has been updated!" })
-                    if (props.setOutfit) { props.setOutfit(data) }
+                    if (props.setOutfit) {  props.setOutfit(data)  }
 
                     setSnackOpen(true);
                     handleClosing();
@@ -89,27 +131,27 @@ export default function OutfitEditor(props) {
 
     return (
         <React.Fragment>
-            <Dialog fullWidth maxWidth="sm" open={props.open} onClose={handleClosing}>
+            <Dialog fullWidth maxWidth="lg" open={props.open} onClose={handleClosing}>
                 <DialogTitle>Editing {name}</DialogTitle>
                 <DialogContent>
-                    <table style={{width:'100%'}}>
-                        {props.vertical ? 
+                    <table style={{ width: '100%' }}>
+                        {props.vertical ?
                             <tr>
-                                <td style={{textAlign:'center'}}>
-                                    <img src={imageURL === "" ? "/images/outfit.png" : imageURL} alt="Profile" width="200px"    />
-                                    <br/>
+                                <td style={{ textAlign: 'center' }}>
+                                    <img src={imageURL === "" ? "/images/outfit.png" : imageURL} alt="Profile" width="200px" />
+                                    <br />
                                 </td>
                             </tr>
-                        : ""}
+                            : ""}
                         <tr>
                             <td>
                                 <TextField label="Name" value={name} onChange={(event) => setName(event.target.value)} fullWidth
                                     style={{ marginTop: "5px", marginBottom: "5px" }} />                            </td>
                             {
-                                props.vertical ? "" : 
-                                <td rowSpan="3" style={{width:'225px'}}>
-                                    <img src={imageURL === "" ? "/images/outfit.png" : imageURL} alt="Profile" width="200px" style={{ marginLeft: "25px", marginRight: "10px"}} />
-                                </td>
+                                props.vertical ? "" :
+                                    <td rowSpan="3" style={{ width: '225px' }}>
+                                        <img src={imageURL === "" ? "/images/outfit.png" : imageURL} alt="Profile" width="200px" style={{ marginLeft: "25px", marginRight: "10px" }} />
+                                    </td>
                             }
                         </tr>
                         <tr>
@@ -125,6 +167,44 @@ export default function OutfitEditor(props) {
                             </td>
                         </tr>
                     </table>
+                    <br />
+                    <Divider />
+                    <br />
+                    <Grid container spacing={2}>
+
+                        <Grid item xs>
+                            <WearableMicroCard wearable={props.outfit.shirt} setWearable={setShirt} type={'shirt'} washable />
+                        </Grid>
+
+                        {props.outfit.overshirts.map(o => (
+                            <Grid item xs>
+                                <WearableMicroCard wearable={o} setWearable={setOvershirt} type='Overshirt' washable />
+                            </Grid>
+                        ))}
+
+                        <Grid item xs>
+                            <WearableMicroCard wearable={props.outfit.belt} type={'belt'} />
+                        </Grid>
+
+                        <Grid item xs>
+                            <WearableMicroCard wearable={props.outfit.pants} setWearable={setPants} type={'pants'} washable />
+                        </Grid>
+
+                        <Grid item xs>
+                            <WearableMicroCard wearable={props.outfit.socks} setWearable={setSocks} type={'socks'} washable />
+                        </Grid>
+
+                        <Grid item xs>
+                            <WearableMicroCard wearable={props.outfit.shoes} type={'shoes'} />
+                        </Grid>
+
+                        {props.outfit.accessories.map(o => (
+                            <Grid item xs>
+                                <WearableMicroCard wearable={o} type='Accessory'/>
+                            </Grid>
+                        ))}
+
+                    </Grid>
                 </DialogContent>
                 <DialogActions>
                     <Button onClick={handleOK}>OK</Button>
