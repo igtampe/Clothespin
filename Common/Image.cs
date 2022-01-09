45e7a34d41794e871611d86ce8366e5332b72b16
Ignacio Tampe <Igtampe@gmail.com>
2022-01-09 14:42:03 -0400

    Create image object to store images in the DB

---
+++ Common/Image.cs
@@ -0,0 +1,13 @@
+﻿namespace Igtampe.Clothespin.Common {
+
+    /// <summary>Image class that holds an image in the database</summary>
+    public class Image : Identifiable {
+
+        /// <summary>Data of the image</summary>
+        public byte[]? Data { get; set; }
+
+        /// <summary>MIME Type of this image (image/png, image/jpeg, or image/gif)</summary>
+        public string? Type { get; set; }
+
+    }
+}
